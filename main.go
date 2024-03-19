b4f381830838a7f4d392d359410f7fca76c0e1c7
xTavi <tavitavi62@gmail.com>
2024-03-19 02:06:56 +0200

    Finalized post method for habit creation

--- main.go
+++ main.go
@@ -32,11 +32,28 @@ type HabitRecord struct {
 	Note    string    `json:"note"`
 }
 
+type HttpResponseError struct {
+	ErrorMessage string `json:"errorMessage"`
+}
+
 func main() {
 
-	http.HandleFunc("POST /habits", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("POST /habits", handleHabitCreation)
+	http.HandleFunc("GET /habits", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("%s %s", r.Method, r.URL)
+		fmt.Fprintf(w, "Hit the habits endpoint")
+	})
 
-		// respond to the client with the error message and a 400 status code.
+	err := http.ListenAndServe(portNumber, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleHabitCreation(w http.ResponseWriter, r *http.Request) {
+	{
+		printRequestInfo(*r)
+		w.Header().Set("Content-Type", "application/json")
 		var habit Habit
 		err := json.NewDecoder(r.Body).Decode(&habit)
 
@@ -47,28 +64,26 @@ func main() {
 		}
 
 		if habit.Name == "" {
-			http.Error(w, json.Marshal(name: "Name is required"), http.StatusBadRequest)
+			message, err := json.Marshal(HttpResponseError{ErrorMessage: "Name is required"})
+
+			if err != nil {
+				log.Default().Printf("Error marshalling response: %v", err)
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			http.Error(w, string(message[:]), http.StatusBadRequest)
 			return
 		}
 
 		habit.ID = guid.New().String()
 
-		fmt.Printf("%s %s \n", r.Method, r.URL)
 		log.Default().Print(habit)
 		fmt.Println(habit.ID)
 		fmt.Println(habit.Name)
-		fmt.Fprintf(w, "Hit the habits endpoint")
-	})
-
-	http.HandleFunc("GET /habits", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("%s %s", r.Method, r.URL)
-		fmt.Fprintf(w, "Hit the habits endpoint")
-	})
-
-	//TODO How can I add another route like GET /habits and POST /habits
-
-	err := http.ListenAndServe(portNumber, nil)
-	if err != nil {
-		log.Fatal(err)
 	}
+}
+
+func printRequestInfo(r http.Request) {
+	fmt.Printf("%s %s \n", r.Method, r.URL)
 }
