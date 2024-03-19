d97187d1e3d94a13a39e3d2c939e25cea68cd3c0
xTavi <tavitavi62@gmail.com>
2024-03-19 03:20:59 +0200

    Created HabitRecordCreation

--- main.go
+++ main.go
@@ -39,6 +39,7 @@ type HttpResponseError struct {
 func main() {
 
 	http.HandleFunc("POST /habits", handleHabitCreation)
+	http.HandleFunc("POST /habits/{id}/track", handleHabitRecordCreation)
 	http.HandleFunc("GET /habits", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("%s %s", r.Method, r.URL)
 		fmt.Fprintf(w, "Hit the habits endpoint")
@@ -51,37 +52,88 @@ func main() {
 }
 
 func handleHabitCreation(w http.ResponseWriter, r *http.Request) {
-	{
-		printRequestInfo(*r)
-		w.Header().Set("Content-Type", "application/json")
-		var habit Habit
-		err := json.NewDecoder(r.Body).Decode(&habit)
+	printRequestInfo(*r)
+	w.Header().Set("Content-Type", "application/json")
+	var habit Habit
+	err := json.NewDecoder(r.Body).Decode(&habit)
+
+	if err != nil {
+		log.Default().Printf("Error decoding request body: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if habit.Name == "" {
+		message, err := json.Marshal(HttpResponseError{ErrorMessage: "Name is required"})
 
 		if err != nil {
-			log.Default().Printf("Error decoding request body: %v", err)
+			log.Default().Printf("Error marshalling response: %v", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
-		if habit.Name == "" {
-			message, err := json.Marshal(HttpResponseError{ErrorMessage: "Name is required"})
+		http.Error(w, string(message[:]), http.StatusBadRequest)
+		return
+	}
 
-			if err != nil {
-				log.Default().Printf("Error marshalling response: %v", err)
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
+	habit.ID = guid.New().String()
 
-			http.Error(w, string(message[:]), http.StatusBadRequest)
+	log.Default().Print(habit)
+	fmt.Println(habit.ID)
+	fmt.Println(habit.Name)
+
+	if data, err := json.Marshal(habit); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		w.WriteHeader(http.StatusCreated)
+		w.Write(data)
+		r.Body.Close()
+	}
+
+}
+
+func handleHabitRecordCreation(w http.ResponseWriter, r *http.Request) {
+	printRequestInfo(*r)
+	w.Header().Set("Content-Type", "application/json")
+	incomingId := r.PathValue("id")
+	if !guid.IsGuid(incomingId) {
+		message, err := json.Marshal(HttpResponseError{ErrorMessage: "Invalid habitId"})
+
+		if err != nil {
+			log.Default().Printf("Error marshalling response: %v", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
-		habit.ID = guid.New().String()
-
-		log.Default().Print(habit)
-		fmt.Println(habit.ID)
-		fmt.Println(habit.Name)
+		http.Error(w, string(message[:]), http.StatusBadRequest)
+		return
 	}
+
+	var habitRecord HabitRecord
+	err := json.NewDecoder(r.Body).Decode(&habitRecord)
+
+	if err != nil {
+		log.Default().Printf("Error decoding request body: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	habitRecord.ID = guid.New().String()
+	habitRecord.HabitId = incomingId
+	habitRecord.Date = time.Now()
+
+	log.Default().Print("Created habitRecord: ", habitRecord)
+	fmt.Println(habitRecord.ID)
+	fmt.Println(habitRecord.HabitId)
+	fmt.Println(habitRecord.Date)
+	fmt.Println(habitRecord.Note)
+
+	createdHabitRecord, err := json.Marshal(habitRecord)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(createdHabitRecord))
 }
 
 func printRequestInfo(r http.Request) {
